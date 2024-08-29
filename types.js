e1325b46cec8356b4ff7e6ed8e78c5c25b727a6e
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-08-29 16:00:57 +0600

    Types added.

---
+++ types.js
@@ -0,0 +1,18 @@
+// String
+var name = "My name is : Tonmoy sutradhar.";
+var address = "My address is : Tangail, Dhaka.";
+var country = "My country name is: Bangladesh.";
+console.log(name, address, country);
+console.log(typeof country);
+
+// Number/ Integer
+var int = 77;
+console.log(int);
+console.log(typeof int);
+
+// Boolean
+var isRich = true;
+console.log(isRich);
+var passed = false;
+console.log(passed);
+console.log(typeof passed);
