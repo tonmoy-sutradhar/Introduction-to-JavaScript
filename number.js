0466ba2c71b9391024272bb8c4f4f7269aa64edb
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-08-29 16:46:29 +0600

    Numbering and number keyword added.

---
+++ number.js
@@ -0,0 +1,33 @@
+var orangePrice = 20;
+var chocolate = 0.4;
+var apple = 40;
+var Banana = parseInt("200");
+var Mango = parseInt("jdskjfhskjhf");
+var kola = parseInt("20.5");
+var lichu = parseFloat("20.5");
+
+console.log(orangePrice + apple + Banana);
+console.log(apple + Mango); // NaN return korbe "Not a Number"
+console.log(chocolate);
+console.log(typeof chocolate);
+console.log(typeof Mango);
+console.log(Mango);
+console.log(lichu);
+console.log(kola);
+console.log("**********************************");
+
+// ****************************************
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+
+// console.log(total);
+console.log(typeof total.toFixed(2));
+console.log("**********************************");
+
+// ********************************************
+const num1 = 50 / 0;
+const num2 = -50 / 0;
+console.log(num1);
+console.log(num2);
---
+++ numbering.js
@@ -0,0 +1,32 @@
+// variable/boolean do not write this
+var flase = 398; //x
+var var = 40; //x
+
+// No space or gap in variable name
+ var is happy = flase; 
+var ishappy = true;  //Right
+
+// No quote
+var "address" = "Dhakas"; 
+
+// Can not start with a number. but number other than the first letter is allowed.
+var money = 39;
+var money2 = 9393;
+var 3money = 00; //x
+
+// Name is case sensitive
+address
+address
+ADDRESS
+addRess
+ADDress
+
+// How to write a long variable name
+
+var my current home address = "Tangail,dhaka"; //x
+var my currenthomeaddress = "Tangail,dhaka";  //x
+var my current_home_address = "Tangail,dhaka"; //snake case
+var myCurrentHomeAddress = "Tangail,dhaka"; //camel case
+var MyCurrentHomeAddress = "Tangail,dhaka"; //pascal case
+
+
